a1a70e0697d23ccb487f3716e9cd50e92338e6c5
iamranga <rangarajan.ramadass@cognizant.com>
2017-07-05 20:05:33 +0530

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
-sudo su
-git clone CapstoneProject resource-app
+git clone CapstoneProject
 echo "cloning done"
 echo "About to print pwd1 value -"
 echo $(pwd)
-chmod 777 resource-app
-cd resource-app
+cd CapstoneProject
 echo "About to print pwd2 value -"
 echo $(pwd)
 mvn clean install -e -DskipTests=true || ErrorHandler "Build Failed for test"
-chmod o+x target
 echo $(ls -lart)
 echo $(pwd)
 cd target

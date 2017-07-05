39671718d39b705d1bce2b239be123cb4c028baf
rangarajan <rangarajan@rangarajans-MacBook-Pro.local>
2017-07-06 02:34:48 +0530

    updated build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 #git clone rr_git
 #echo "cloning done"
-sudo su
 echo "About to print pwd1 value -"
 echo $(pwd)
-chmod 777 rr_git
-chmod 777 resource-app
-cd rr_git
 echo "About to print pwd2 value -"
 echo $(pwd)
 mvn clean install -e -DskipTests=true || ErrorHandler "Build Failed for test"

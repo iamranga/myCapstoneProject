a919446ab5c2ccf0105f9d72a0ad02aebab53873
iamranga <rangarajan.ramadass@cognizant.com>
2017-07-06 02:22:41 +0530

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 #git clone rr_git
 #echo "cloning done"
+set -e -x
 sudo su
 echo "About to print pwd1 value -"
 echo $(pwd)
+chmod 777 rr_git
+chmod 777 resource-app
 cd rr_git
 echo "About to print pwd2 value -"
 echo $(pwd)
